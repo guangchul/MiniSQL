7c8a3b58090fd4fe29ea7d590282b0d0c45ea3bc
guangchul <choiguangchul@gmail.com>
2023-04-29 14:48:24 +0800

    machine.h

---
+++ global/machine.h
@@ -0,0 +1,15 @@
+/*
+ * machine.h
+ *
+ *  Created on: 2023年4月29日
+ *      Author: Administrator
+ */
+
+#ifndef GLOBAL_MACHINE_H_
+#define GLOBAL_MACHINE_H_
+
+
+#define WIN 1
+
+
+#endif /* GLOBAL_MACHINE_H_ */
